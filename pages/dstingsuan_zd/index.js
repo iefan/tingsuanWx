09139c759f7ecc44244d3372b4c9bd551a2cfcad
iefan <mybsppp@163.com>
2019-05-14 07:12:57 +0800

    修正变量名

--- pages/dstingsuan_zd/index.js
+++ pages/dstingsuan_zd/index.js
@@ -86,7 +86,7 @@ Page({
     if (this.data.btnText === "显示答案"){
       // console.log(this.data.numberArrayList, '------')
       this.data.btnText = "开始听题";
-      this.data.numberArray = [];
+      this.data.numberArrayList = [];
       this.data.indexNumberArray = [];
       this.setData({
         numberArray: this.data.numberArrayList,
@@ -205,7 +205,7 @@ Page({
 
     this.registerAudioContext();
 
-    this.data.numberArray = [];
+    this.data.numberArrayList = [];
     this.data.indexNumberArray = [];
     this.setData(
       {
