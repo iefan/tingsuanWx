1278273ef8c17c667787149f81d9a4179ef8dcdf
iefan <mybsppp@163.com>
2019-05-21 17:02:56 +0800

    测试百度语音合成接口

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,11 +5,103 @@ const app = getApp()
 Page({
   data: {
     motto: '反馈请扫描',
+    mytok:[]   
     // userInfo: {},
     // hasUserInfo: false,
     // canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
+  hechengceshi: function () {
+    
+    //合成语音
+    // this.ceshi();
+    let that=this;
+    console.log("hecheng====", this.data.mytok)
+    wx.request({
+      url: 'https://tsn.baidu.com/text2audio',
+      method: 'POST',
+      data: {
+        tex: '1+1=2',
+        lan: 'zh',
+        tok: this.data.mytok['access_token'],
+        cuid: "Iklsljfdskl21",
+        ctp: 1,
+        spd: 5,
+      },
+      // header: {
+      //   'content-type': 'application/json'
+      // },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        console.log("audio=================="),
+          that.innerAudioContext.src = res.data;
+          that.innerAudioContext.play()
+          // console.log(res.data)
+      }
+    })
+
+  },
+
+  ceshi: function(){    
+    var tex, tok, cuid, ctp, lan='zh', spd;
+    let that = this;
+
+    //get method
+    // wx.request({
+    //   url: 'https://aip.baidubce.com/oauth/2.0/token', 
+    //   data: {
+    //     grant_type: 'client_credentials',
+    //     client_id: 'LGRH7Xu607DIz6OsGnoef7jG',
+    //     client_secret: '0g9YbnhvOjp8f0FGjDBALE0NBXcnLQmO'
+    //   },
+    //   header: {
+    //     'content-type': 'application/json'
+    //   },
+    //   success: function (res) {
+    //     console.log(res.data),
+    //     console.log(res.data['access_token']),
+    //     console.log(res.data['expires_in'])
+    //   }
+    // }),
+
+    wx.request({
+      url: 'https://aip.baidubce.com/oauth/2.0/token',
+      method: 'POST',
+      data: {
+        grant_type: 'client_credentials',
+        client_id: 'LGRH7Xu607DIz6OsGnoef7jG',
+        client_secret: '0g9YbnhvOjp8f0FGjDBALE0NBXcnLQmO'
+      },
+      // header: {
+      //   'content-type': 'application/json'
+      // },
+      header:{
+        'content-type':'application/x-www-form-urlencoded'
+      },      
+      success: function (res) {
+        // that.setData({
+        //   mytok:res.data
+        // })
+        // console.log("post=================="),
+        // console.log(res.data),
+        // // console.log(res.data['access_token']),
+        // // console.log("tok====:", tok)
+        that.setData({
+          mytok : res.data
+        })
+        console.log(res.data['expires_in'])
+        // console.log(encodeURI("百度你好")),
+        // console.log(encodeURI(encodeURI("百度你好")))
+      }
+    })
+
+    // console.log("items====:", this.data.mytok);
+  },
+
+  
+
   Grade99Tinsuan: function () {
     wx.navigateTo({
       url: '../dstingsuan_99times/index'
@@ -34,6 +126,7 @@ Page({
   },
 
   onLoad: function () {
+    this.innerAudioContext = wx.createInnerAudioContext();
     // if (app.globalData.userInfo) {
     //   this.setData({
     //     userInfo: app.globalData.userInfo,
